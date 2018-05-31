42dae2685bfaf4d9c32fa83b99998f4789e55ddf
andrew.roldugin <andrew.roldugin@gmail.com>
2018-05-31 17:59:18 +0300

    Add tests stub for TxHandler.handleTxs.

--- assignment1/run_tests.bat
+++ assignment1/run_tests.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-javac -cp src;test;lib/junit-4.12.jar test/IsValidTest.java
+javac -cp src;test;lib/junit-4.12.jar test/IsValidTest.java test/HandleTest.java
 IF %ERRORLEVEL% EQU 0 ^
-java -cp src;test;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore IsValidTest
+java -cp src;test;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore IsValidTest HandleTest
